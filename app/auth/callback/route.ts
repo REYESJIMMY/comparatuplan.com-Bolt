f89067c7f1115a0fb6d811c0ead0a3c6b546dafc
REYESJIMMY <113445308+REYESJIMMY@users.noreply.github.com>
2026-04-15 21:46:17 -0500

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: Request) {
   const next = searchParams.get("next") ?? "/";
 
   if (code) {
-    const cookieStore = await cookies(); // await necesario en Next.js 15+
+    const cookieStore = await cookies();
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
